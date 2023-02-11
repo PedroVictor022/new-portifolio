22a7d7e7d0a84276d6fdd8b552b29d4773c04c10
Pedro Victor <pedro20002victor@gmail.com>
2023-02-11 11:27:06 -0300

    home apresentation

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,16 +1,63 @@
+import NextLink from 'next/link';
+import { useState } from 'react';
+import { FaBars, FaTimes } from "react-icons/fa";
+
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const links = [
+    {
+      id: 1,
+      link: 'Home',
+      href: '/'
+    },
+    {
+      id: 2,
+      link: 'Sobre',
+      href: "/about"
+    },
+    {
+      id: 3,
+      link: 'Projetos',
+      href: '/projects'
+    }
+  ]
+
   return (
-    <div className="flex justify-between items-center w-full h-24 px-4 text-white bg-black fixed">
+    <div className="flex justify-between items-center w-full h-24 px-4 text-white bg-black fixed border-purple-700 border-b-2">
       <div>
-        <h1 className="text-5xl font-signature ml-2">Pedro Victor</h1>
+        <h1 className="text-5xl font-signature ml-2 ">Pedro Victor</h1>
       </div>
 
-      <div className="flex">
-        <a className="px-4 cursor-pointer capitalize font-medium text-gray-400 hover:scale-150 duration-200">Home</a>
-        <a className="px-4 cursor-pointer capitalize font-medium text-gray-400 hover:scale-150 duration-200">Sobre</a>
-        <a className="px-4 cursor-pointer capitalize font-medium text-gray-400 hover:scale-150 duration-200">Projetos</a>
+      <div className="hidden md:flex ">
+        {links.map(item => (
+          <NextLink key={item.id} href={item.href} className="px-4 cursor-pointer capitalize font-medium text-gray-400 hover:scale-150 duration-200">{item.link}</NextLink>
+        ))}
       </div>
-      
+
+      <div
+        className='cursor-pointer md:hidden pr-4 z-10 text-gray-600'
+        onClick={() => setNav(!nav)}
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {
+        nav && (
+          <div className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
+            {
+              links.map(item => (
+                <NextLink href={item.href} key={item.id} className='px-4 cursor-pointer capitalize py-6 text-4xl'>{item.link}</NextLink>
+              ))
+            }
+
+
+          </div>
+
+        )
+      }
+
+
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Navbar from '@/components/navbar'
+import Main from '@/components/home'
 import Head from 'next/head'
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Main />
     </div>
   )
 }
