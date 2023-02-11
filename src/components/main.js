33f36a8903c4b0f29fd25d27725d7077f096b669
Pedro Victor <pedro20002victor@gmail.com>
2023-02-10 21:59:46 -0300

    refactor

---
+++ src/components/main.js
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react"
+
+export const Main = ({ children, router }) => {
+  return (
+    <Box as="main" pb={8}>
+      <Head>
+        
+      </Head>
+    </Box>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
 import Head from 'next/head'
 
-export default function Home() {
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import Navbar from '@/components/navbar/navbar'
+import { Container } from '@chakra-ui/react'
+
+
+export default function Home({ children, router }) {
   return (
     <>
       <Head>
@@ -9,6 +15,15 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Pedro Victor - Homepage</title>
       </Head>
+      {/* CREATE A NAVBAR */}
+      {/* ADDING ROUTER PROPS = router.asPath */}
+
+      <Navbar path={router} />
+
+      <Container maxW="container.md" pt={7}>
+        {children}
+      </Container>
+
     </>
   )
 }
