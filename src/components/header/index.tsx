f9f84a0830f8bd564e8ff177d8a62520b72619b5
Pedro Victor <pedro20002victor@gmail.com>
2023-02-25 23:17:09 -0300

    fix details

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,38 +2,34 @@ import styled from "styled-components";
 import Title from "../Title/principalTitle";
 
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { WrapLine } from "../../styles/utils";
 
 export default function Header() {
   return (
     <HeaderComponent>
       <Title />
-      <div className="box_1">
-        <p>
-          Bem vindo ao meu Portifolio, aqui você encontra um pouco sobre mim e
-          meus projetos.
-        </p>
-        <div className="box_links">
-          <LinkComponents color="#2B2D32">
-            <a
-              href="https://www.linkedin.com/in/pedro-victor-75440321a/"
-              target="_blank"
-            >
-              Linkedin
-            </a>
-            <AiFillLinkedin className="icon" />
-          </LinkComponents>
+      <div className="box_links">
+        <LinkComponents color="#2B2D32">
+          <a
+            href="https://www.linkedin.com/in/pedro-victor-75440321a/"
+            target="_blank"
+          >
+            Linkedin
+          </a>
+          <AiFillLinkedin className="icon" />
+        </LinkComponents>
 
-          <LinkComponents color="#5658DD">
-            <a
-              href="https://www.linkedin.com/in/pedro-victor-75440321a/"
-              target="_blank"
-            >
-              Github
-            </a>
-            <AiFillGithub className="icon" />
-          </LinkComponents>
-        </div>
+        <LinkComponents color="#5658DD">
+          <a
+            href="https://www.linkedin.com/in/pedro-victor-75440321a/"
+            target="_blank"
+          >
+            Github
+          </a>
+          <AiFillGithub className="icon" />
+        </LinkComponents>
       </div>
+      <WrapLine />
     </HeaderComponent>
   );
 }
@@ -42,9 +38,8 @@ const HeaderComponent = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  .box_1 {
-  }
   .box_links {
+    padding-top: 2rem;
     display: flex;
     gap: 1rem;
   }
@@ -58,6 +53,7 @@ const LinkComponents = styled.div`
   padding: 1rem;
   border-radius: 5px;
   background-color: ${(props) => props.color || "#fff"};
+  
   &:hover {
     a {
       color: #fff;
@@ -73,5 +69,6 @@ const LinkComponents = styled.div`
   .icon {
     font-size: 29px;
     background-color: transparent;
+    color: #fff;
   }
 `;
