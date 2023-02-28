5e831425ce177c14a38c4e8566ab8627e79c8a0e
Pedro Victor <pedro20002victor@gmail.com>
2023-02-28 08:53:58 -0300

    up

--- src/styles/Global.ts
+++ src/styles/Global.ts
@@ -3,13 +3,15 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
   :root {
     --primary-color: #5658DD;
+    --secondary-color:#222222;
+    --footer-color:#17171C;
   }
 
   * {
     padding: 0;
     margin: 0;
     box-sizing: border-box;
-    background-color: #222;
+    background-color: var(--secondary-color);
     font-family: 'Montserrat', sans-serif;
   }
 `;
