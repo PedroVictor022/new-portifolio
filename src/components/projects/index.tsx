52a71da2d14ad8711bccc0b88fc0beeaf7395823
PedroVictor022 <pedro20002victor@gmail.com>
2023-03-03 09:14:46 -0300

    remove files

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,63 +1,12 @@
 import styled from "styled-components";
 import Title from "../Title/principalTitle";
-import ProjectsItem from "./projectItem";
 
 export default function MyProjects() {
   return (
-    <>
-      <ContainerProjects>
-        <Title 
-          text1="Meus"
-          text2="Projetos"
-        />
-        <div
-          className="project-items"
-        >
-          <ProjectsItem 
-            title="Steam Redesing"
-            description="Uma nova interface para o site da Steam."
-            link="https://steam-new-desing.vercel.app/"
-          />
-
-          <ProjectsItem 
-            title="Ecommerce"
-            description="Ecommerce criado do total zero, feito com ReactJS e seus hooks."
-            link="https://ecommercegeek.vercel.app/"
-          />
-
-          <ProjectsItem 
-            title="RAM Display"
-            description="Aplicativo desktop criado com ElectronJS, o app monitora o uso de memoria RAM do computador, exibindo as estatisticas em tempo real."
-            link="https://steam-new-desing.vercel.app/"
-          />
-
-          <ProjectsItem 
-            title="Banco de Imagens"
-            description="O banco de imagens é um trabalho freelancer onde eu estou programando um ecommerce de videos de alta qualidade. Em breve disponivel para acesso."
-            link="*"
-          />
-        </div>
-      </ContainerProjects>
-    </>
+    
   )
 }
 
 const ContainerProjects = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  margin-top: 2rem;
-
-  .project-items {
-    margin-top: 1rem;
-    margin-bottom: 4rem;
-    display: grid;
-    grid-template-columns: 2fr 2fr;
-    gap: 2rem;
-    @media (max-width:700px) {
-      grid-template-columns: 2fr;
-    }
-  }
 
 `;
