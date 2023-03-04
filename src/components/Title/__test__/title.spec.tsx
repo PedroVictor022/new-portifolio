239cd909a454c1200395662764644119443ae2df
PedroVictor022 <pedro20002victor@gmail.com>
2023-03-03 21:09:37 -0300

    up all alterations

---
+++ src/components/Title/__test__/title.spec.tsx
@@ -0,0 +1,5 @@
+describe("Test Title component", () => {
+  it('test render', () => {
+    expect(1).toBe(1);
+  })
+})
--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -20,26 +20,37 @@ export default function Skills() {
       <Title text1="Skills" />
 
       <div className="projectItem">
-        <DiReact />
-        <DiJavascript1 />
-        <TbBrandNextjs />
-        <GrNode />
-        <SiGraphql />
-        <SiNestjs />
-        <SiVuedotjs />
-        <SiMongodb />
-        <SiPostgresql />
+        <DiReact className="icon" />
+        <DiJavascript1 className="icon" />
+        <TbBrandNextjs className="icon" />
+        <GrNode className="icon" />
+        <SiGraphql className="icon" />
+        <SiNestjs className="icon" />
+        <SiVuedotjs className="icon" />
+        <SiMongodb className="icon" />
+        <SiPostgresql className="icon" />
       </div>
     </SkillsContainer>
   );
 }
 
 const SkillsContainer = styled(Layout)`
-  background-color: green;
+  /* background-color: green; */
+  height: 60vh;
   .projectItem {
+    margin-right: 25px;
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(3, 2fr);
     gap: 4rem;
-    background-color: red;
+    /* background-color: red; */
+
+    .icon {
+      font-size: 90px;
+      background-color: red;
+      padding: 0.2rem;
+      cursor: pointer;
+      border-radius: 0.4rem;
+    }
+
   }
 `;
